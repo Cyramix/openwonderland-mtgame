04968f2edbaf563c1bea6d2e404e1d4be55af7fb
yensid <yensid@8c24ba5a-4cb4-4c0c-af24-a78907945f20>
2008-10-14 03:19:54 +0000

    Initial Import

---
+++ org/jdesktop/mtgame/JBulletDynamicCollisionSystem.java
@@ -0,0 +1,198 @@
+/*
+ * Copyright 2008 Sun Microsystems, Inc.  All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Sun Microsystems nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.jdesktop.mtgame;
+
+import java.util.ArrayList;
+
+import com.bulletphysics.collision.broadphase.BroadphaseInterface;
+import com.bulletphysics.collision.broadphase.AxisSweep3;
+import com.bulletphysics.collision.dispatch.CollisionDispatcher;
+import com.bulletphysics.collision.dispatch.DefaultCollisionConfiguration;
+import com.bulletphysics.collision.dispatch.CollisionWorld;
+import com.bulletphysics.dynamics.DiscreteDynamicsWorld;
+import com.bulletphysics.dynamics.DynamicsWorld;
+import com.bulletphysics.dynamics.constraintsolver.ConstraintSolver;
+import com.bulletphysics.dynamics.constraintsolver.SequentialImpulseConstraintSolver;
+import com.bulletphysics.dynamics.RigidBody;
+import com.bulletphysics.collision.shapes.CollisionShape;
+import com.jmex.terrain.TerrainPage;
+
+import javax.vecmath.Vector3f;
+import com.jme.math.Ray;
+
+import com.jme.scene.Node;
+import com.jme.scene.Spatial;
+import com.jme.scene.GeometricUpdateListener;
+import com.jme.scene.TriMesh;
+
+/**
+ * This collision system uses jme methods to implement collision queries
+ * @author Doug Twilleager
+ */
+public class JBulletDynamicCollisionSystem extends CollisionSystem implements GeometricUpdateListener {
+    /**
+     * Some JBullet Collision Objects
+     */
+    private BroadphaseInterface overlappingPairCache = null;
+    private CollisionDispatcher dispatcher = null;
+    private DefaultCollisionConfiguration collisionConfiguration = null;
+    private ConstraintSolver solver = null;
+    private DynamicsWorld dynamicsWorld = null;
+    
+    /**
+     * The list of collision components to be used for collision queries
+     */
+    private ArrayList collisionComponents = new ArrayList();
+    
+    /**
+     * The default constructor
+     */
+    public JBulletDynamicCollisionSystem() {
+        collisionConfiguration = new DefaultCollisionConfiguration();
+        dispatcher = new CollisionDispatcher(collisionConfiguration);
+        Vector3f worldAabbMin = new Vector3f(-10000, -10000, -10000);
+        Vector3f worldAabbMax = new Vector3f(10000, 10000, 10000);
+        overlappingPairCache = new AxisSweep3(worldAabbMin, worldAabbMax, 1000);
+        solver = new SequentialImpulseConstraintSolver();
+        dynamicsWorld = new DiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+        dynamicsWorld.setGravity(new Vector3f(0f, -10f, 0f));
+    }
+    
+    /**
+     * This creates a default collision component object.  The default collision
+     * component object exposes jME picking/collision API's
+     */
+    public JBulletCollisionComponent createCollisionComponent(Node node) {
+        JBulletCollisionComponent cc = new JBulletCollisionComponent(this, node);
+        // Need to wait for node to initialize, so just return.
+        return (cc);
+    }
+    
+    /**
+     * This creates a default collision component object.  The default collision
+     * component object exposes jME picking/collision API's
+     */
+    public JBulletCollisionComponent createCollisionComponent(CollisionShape cs) {
+        JBulletCollisionComponent cc = new JBulletCollisionComponent(this, cs);
+        // Need to wait for node to initialize, so just return.
+        return (cc);
+    }
+
+        
+    /**
+     * This creates a default collision component object.  The default collision
+     * component object exposes jME picking/collision API's
+     */
+    public JBulletCollisionComponent createCollisionComponent(TriMesh mesh) {
+        JBulletCollisionComponent cc = null;
+        
+        synchronized (dynamicsWorld) {
+            cc = new JBulletCollisionComponent(this, mesh);
+        }
+        // Need to wait for node to initialize, so just return.
+        return (cc);
+    }
+    
+    /**
+     * Remove the JME collision component from collision consideration
+     * @param cc
+     */
+    public void removeCollisionComponent(JBulletCollisionComponent cc) {
+        synchronized (dynamicsWorld) {
+            collisionComponents.remove(cc);         
+            if (cc.getPhysicsComponent() != null) {
+                dynamicsWorld.addRigidBody((RigidBody) cc.getCollisionObject());
+            } else {
+                cc.getNode().removeGeometricUpdateListener(this);
+                dynamicsWorld.addCollisionObject(cc.getCollisionObject());
+            }
+        }
+    }
+    
+    public void addCollisionComponent(CollisionComponent cc) {
+        JBulletCollisionComponent jcc = (JBulletCollisionComponent) cc;
+        synchronized (dynamicsWorld) {
+            jcc.initialize();
+
+            if (jcc.getPhysicsComponent() != null) {
+                dynamicsWorld.addRigidBody((RigidBody) jcc.getCollisionObject());
+            } else {
+                if (jcc.getNode() != null && jcc.getPhysicsComponent() == null) {
+                    jcc.getNode().addGeometricUpdateListener(this);
+                }
+                dynamicsWorld.addCollisionObject(jcc.getCollisionObject());
+            }
+            collisionComponents.add(cc);
+        }
+    }
+    /**
+     * A pick routine, which will pick against every scene rendered
+     */
+    public void rayTest(Vector3f from, Vector3f to, CollisionWorld.RayResultCallback result) {
+        synchronized (dynamicsWorld) {
+            dynamicsWorld.rayTest(from, to, result);
+        }
+    }
+    
+    public void geometricDataChanged(Spatial s) {
+        synchronized (dynamicsWorld) {
+            for (int i=0; i<collisionComponents.size(); i++) {
+                JBulletCollisionComponent jcc = (JBulletCollisionComponent) collisionComponents.get(i);
+                if (jcc.getNode() == s) {
+                    jcc.nodeChanged();
+                    break;
+                }
+            }
+        }
+    }
+    
+    /**
+     * Get the dynamics world - used by the physics system
+     */
+    DynamicsWorld getDynamicsWorld() {
+        return (dynamicsWorld);
+    }
+        
+    /**
+     * TODO: Need to implement this
+     */
+    public PickInfo pickAllEyeRay(Ray eyeRay, CameraComponent cc, boolean geometryPick, boolean interpolataData) {
+        return (null);    
+    }
+
+    /**
+     * TODO: Need to implement this
+     */
+    public PickInfo pickAllWorldRay(Ray worldRay, boolean geometryPick, boolean interpolataData) {
+        return (null);    
+    }
+}
---
+++ org/jdesktop/mtgame/processor/MouseSelectionProcessor.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright 2008 Sun Microsystems, Inc.  All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Sun Microsystems nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.jdesktop.mtgame.processor;
+
+
+import org.jdesktop.mtgame.*;
+import com.jme.math.Vector3f;
+import com.jme.math.Vector2f;
+import com.jme.math.Ray;
+import com.jme.scene.Node;
+import com.jme.renderer.ColorRGBA;
+import com.jme.scene.shape.Box;
+import com.jme.scene.state.*;
+import com.jme.scene.Geometry;
+import com.jme.bounding.BoundingBox;
+import com.jme.bounding.BoundingVolume;
+
+import com.jme.renderer.Camera;
+
+
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author runner
+ */
+public class MouseSelectionProcessor extends AWTEventProcessorComponent {    
+    /**
+     * Some objects used for selection
+     */
+    private Ray eyeRay = new Ray();
+    private Vector3f origin = new Vector3f(0.0f, 0.0f, 0.0f);
+    private Vector3f direction = new Vector3f(0.0f, 0.0f, -1.0f);
+    private int halfWidth = 0;
+    private int halfHeight = 0;
+    private Camera jmeCamera = null;
+    private OrbitCameraProcessor camera = null;
+    private Vector3f thru = new Vector3f();
+
+    
+    /**
+     * An array list of entities for the currently visible bounds
+     */
+    private ArrayList visibleBounds = new ArrayList();
+       
+    /**
+     * The current camera entity
+     */
+    private Entity cameraEntity = null;
+    private CameraComponent cc = null;
+    int height = 0;
+    
+    /**
+     * The WorldManager
+     */
+    private WorldManager worldManager = null;
+    
+    /**
+     * The collision component for this processor
+     */
+    private JMECollisionSystem collisionSystem = null;
+    
+    /**
+     * The default constructor
+     */
+    public MouseSelectionProcessor(AWTInputComponent listener, WorldManager wm, 
+            Entity myEntity, Entity cameraEntity, int windowWidth, int windowHeight,
+            OrbitCameraProcessor ocp) {
+        super(listener);
+        worldManager = wm;
+        collisionSystem = (JMECollisionSystem) 
+                worldManager.getCollisionManager().loadCollisionSystem(JMECollisionSystem.class);
+        cc = (CameraComponent) cameraEntity.getComponent(CameraComponent.class);
+        
+        setEntity(myEntity);
+        this.cameraEntity = cameraEntity;     
+        cc = (CameraComponent) cameraEntity.getComponent(CameraComponent.class);
+        eyeRay.origin.set(origin);
+        eyeRay.direction.set(direction);
+        height = windowHeight;
+        halfWidth = windowWidth/2;
+        halfHeight = windowHeight/2;
+        
+        camera = ocp;
+    }
+    
+    public void initialize() {
+        setArmingCondition(new AwtEventCondition(this));
+    }
+    
+    public void compute(ProcessorArmingCollection collection) {
+        Object[] events = getEvents();
+        
+        for (int i=0; i<events.length; i++) {
+            if (events[i] instanceof MouseEvent) {
+                MouseEvent me = (MouseEvent) events[i];
+                if (me.getID() == MouseEvent.MOUSE_MOVED) {
+                    int x = me.getX();
+                    int y = me.getY();
+                    camera.getPosition(eyeRay.origin);
+                    jmeCamera = cc.getCamera();
+                    Vector2f sc = new Vector2f(x, (halfHeight*2) - y);
+                    jmeCamera.getWorldCoordinates(sc, 0.0f, thru);
+                    
+                    eyeRay.direction.x = thru.x - eyeRay.origin.x;
+                    eyeRay.direction.y = thru.y - eyeRay.origin.y;
+                    eyeRay.direction.z = thru.z - eyeRay.origin.z;
+                    eyeRay.direction.normalize();
+                    
+                    //System.out.println("Thru at: " + thru.x + ", " + thru.y + ", " + thru.z);
+                    //System.out.println("Ray starts at: " + eyeRay.origin.x + ", " + eyeRay.origin.y + ", " + eyeRay.origin.z);
+                    //System.out.println("And has direction: " + eyeRay.direction.x + ", " + eyeRay.direction.y + ", " + eyeRay.direction.z);
+                    clearVisibleBounds();
+                    JMEPickInfo pickInfo = (JMEPickInfo) collisionSystem.pickAllWorldRay(eyeRay, true, false);
+                    //System.out.println(pickInfo.size() + " Geometries were picked");
+                    for (int j = 0; j < pickInfo.size(); j++) {
+                        JMEPickDetails pd = (JMEPickDetails)pickInfo.get(j);
+                        //System.out.println("================ Geometry " + j + " ================");
+                        //System.out.println("\tDistance: " + pd.getDistance());
+                        //System.out.println("\tEntity: " + pd.getEntity());
+                        //System.out.println("\tCollision Component: " + pd.getCollisionComponent());
+                        //System.out.println("\tGeometry: " + pd.getPickData().getTargetMesh());
+                        addToVisibleBounds(pd.getPickData().getTargetMesh());
+                    }
+                }
+            }
+        }
+
+    }
+    
+    private void addToVisibleBounds(Geometry g) {
+        BoundingVolume bv = g.getWorldBound();
+        Entity e = null;
+        Node node = null;
+        ColorRGBA color = new ColorRGBA(1.0f, 0.0f, 0.0f, 0.4f);
+        Box box = null;
+        
+        if (bv instanceof BoundingBox) {
+            BoundingBox bbox = (BoundingBox) bv;
+            Vector3f center = bbox.getCenter();
+            
+            Vector3f extent = bbox.getExtent(null);
+            box = new Box("Bounds", center, extent.x, extent.y, extent.z);
+            box.setDefaultColor(color);
+           
+            e = new Entity("Bounds");
+            node = new Node();
+            node.attachChild(box);
+            RenderComponent rc = worldManager.getRenderManager().createRenderComponent(node);
+            e.addComponent(RenderComponent.class, rc);
+        }
+        
+        ZBufferState buf = (ZBufferState) worldManager.getRenderManager().createRendererState(RenderState.RS_ZBUFFER);
+        buf.setEnabled(true);
+        buf.setFunction(ZBufferState.TestFunction.LessThanOrEqualTo);
+        node.setRenderState(buf);
+
+        BlendState as = (BlendState) worldManager.getRenderManager().createRendererState(RenderState.RS_BLEND);
+        as.setEnabled(true);
+        as.setBlendEnabled(true);
+        as.setSourceFunction(BlendState.SourceFunction.SourceAlpha);
+        as.setDestinationFunction(BlendState.DestinationFunction.OneMinusSourceAlpha);
+        node.setRenderState(as);
+
+        CullState cs = (CullState) worldManager.getRenderManager().createRendererState(RenderState.RS_CULL);
+        cs.setEnabled(true);
+        cs.setCullFace(CullState.Face.Back);
+        node.setRenderState(cs);
+
+        worldManager.addEntity(e);
+        visibleBounds.add(e);
+    }
+    
+    private void clearVisibleBounds() {
+        for (int i=0; i<visibleBounds.size(); i++) {
+            Entity e = (Entity) visibleBounds.get(i);
+            worldManager.removeEntity(e);
+        }
+        visibleBounds.clear();
+    }
+    
+    /**
+     * The commit methods
+     */
+    public void commit(ProcessorArmingCollection collection) {
+        
+    }
+}
