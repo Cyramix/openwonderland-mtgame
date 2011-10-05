bf9cc2253c0ba320e2c50df331358740e42099f4
jonathankap <jonathankap@8c24ba5a-4cb4-4c0c-af24-a78907945f20>
2011-10-05 04:40:43 +0000

    Make TextureRenderBuffer extensible to allow different data layouts

--- trunk/src/classes/org/jdesktop/mtgame/TextureRenderBuffer.java
+++ trunk/src/classes/org/jdesktop/mtgame/TextureRenderBuffer.java
@@ -1,4 +1,33 @@
 /*
+ * Copyright (c) 2011, Open Wonderland Foundation. All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without
+ *    modification, are permitted provided that the following conditions
+ *    are met:
+ *
+ *  . Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  . Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  . Neither the name of Open Wonderland Foundation, nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
  * Copyright (c) 2009, Sun Microsystems, Inc. All rights reserved.
  *
  *    Redistribution and use in source and binary forms, with or without
@@ -161,7 +190,7 @@ public class TextureRenderBuffer extends RenderBuffer {
     private void createTextureObjects(GL gl, DisplaySystem display) {
         // First do the common render target
         assignTextureId(gl, getTexture(), Texture.Type.TwoDimensional);
-        allocateTextureData(gl, getTexture(), Texture.Type.TwoDimensional);
+        textureReadBuffer = allocateTextureData(gl, getTexture(), Texture.Type.TwoDimensional);
         setupState(display, getTexture());
     }
         
@@ -195,7 +224,7 @@ public class TextureRenderBuffer extends RenderBuffer {
     /**
      * Allocate the texture data, based upon what we are doing.
      */
-    void allocateTextureData(GL gl, Texture t, Texture.Type type) {
+    protected ByteBuffer allocateTextureData(GL gl, Texture t, Texture.Type type) {
         int components = GL.GL_RGBA8;
         int format = GL.GL_RGBA;
         int dataType = GL.GL_UNSIGNED_BYTE;
@@ -210,7 +239,7 @@ public class TextureRenderBuffer extends RenderBuffer {
             gl.glGenerateMipmapEXT(GL.GL_TEXTURE_2D);
         }
 
-        textureReadBuffer = BufferUtils.createByteBuffer(getWidth()*getHeight()*4);
+        return BufferUtils.createByteBuffer(getWidth()*getHeight()*4);
     }
     
     /**
